3fa269cab91648ca5f9c0a3a66004c4831f0ad32
Ghanshyam Gupta <147191967+ghanshyamgcs22@users.noreply.github.com>
2024-02-12 23:49:33 +0530

    Create magnetic-force-between-two-balls(leetcode-1552).cpp

---
+++ magnetic-force-between-two-balls(leetcode-1552).cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    int maxDistance(vector<int>& position, int m) {
+    sort(position.begin(), position.end());
+    int low = 1, high = position[position.size() - 1] - position[0];
+    int result ;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        int placed = 1;
+        int lastPlaced = position[0];
+
+        for (int i = 1; i < position.size(); i++) {
+            if (position[i] - lastPlaced >= mid) {
+                placed++;
+                lastPlaced = position[i];
+                if (placed >= m) {
+                    result = mid;
+                    low = mid + 1;
+                    break; // Break from the loop as we've found a possible solution
+                }
+            }
+        }
+
+        if (placed < m) {
+            high = mid - 1;
+        }
+    }
+
+    return result;
+}
+
+};
